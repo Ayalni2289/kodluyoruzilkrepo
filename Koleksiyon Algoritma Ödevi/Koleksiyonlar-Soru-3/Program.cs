415a667432a58b80d8d18389b7118d81bf73b47f
Ayalni2289 <alininspor@hotmail.com>
2024-10-25 17:38:05 +0300

    Sesli Harfleri cümle içerisinde bulan program tamamlandı.

---
+++ Koleksiyon Algoritma Ödevi/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,13 @@
+﻿Odev.SesliHarfler();
+
+class Odev{
+    public static void SesliHarfler(){
+        char[] sesliHarfler = {'a', 'e', 'i', 'o', 'u',};
+        string kelime = Console.ReadLine();
+        for(int i=0;i<kelime.Length;i++){
+            if(Array.IndexOf(sesliHarfler, kelime.ToLower()[i]) != -1){
+                Console.WriteLine(kelime[i]);
+            }
+        }
+    }
+}
