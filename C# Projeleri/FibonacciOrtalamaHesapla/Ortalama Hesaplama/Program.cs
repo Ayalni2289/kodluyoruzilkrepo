d800ae7ad9a963ff15987ad0d098a77c705bd0c6
Ayalni2289 <alininspor@hotmail.com>
2024-11-24 15:09:28 +0300

    Fibonacci Serisi Ortalama Hesaplama Sınıfı

---
+++ C# Projeleri/FibonacciOrtalamaHesapla/Ortalama Hesaplama/Program.cs
@@ -0,0 +1,34 @@
+﻿Fibonacci fibonacci = new Fibonacci();
+fibonacci.Yazdir();
+class Fibonacci{
+    private float ortalamaHesapla(int depth){
+        if(depth==1){
+            return 0;
+        }
+
+            int[] fib = new int[depth];
+            fib[0]= 0;
+            fib[1]= 1;
+            for(int i = 2; i < depth; i++){
+                fib[i] = fib[i-1] + fib[i-2];
+            }
+            int sum = 0;
+            for(int i = 0; i < depth; i++){
+                sum += fib[i];
+            }
+            return (float)sum/depth;
+    }
+
+    public void Yazdir(){
+        Console.WriteLine("Ortalama hesaplanacak fibonacci serisi derinliğini giriniz: ");
+        int derinlik = Convert.ToInt32(Console.ReadLine());
+
+        if(derinlik <= 0){
+            Console.WriteLine("Derinlik 0'dan büyük olmalıdır.");
+        }
+        else{
+            float ortalama = ortalamaHesapla(derinlik);
+            Console.WriteLine("Ortalama: " + ortalama);
+        }
+    }
+}
