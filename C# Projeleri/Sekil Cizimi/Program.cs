dd9bc7a666022e430bdd8aedf8150e20d1af87f1
Ayalni2289 <alininspor@hotmail.com>
2024-11-24 16:31:00 +0300

    Daire Çizme Uygulaması Tamamlandı.

---
+++ C# Projeleri/Sekil Cizimi/Program.cs
@@ -0,0 +1,22 @@
+﻿DaireCiz daireCiz = new DaireCiz();
+daireCiz.Ciz();
+
+class DaireCiz{
+    public void Ciz(){
+        int r, i, j;
+        
+        Console.Write("Dairenin Yarıçapını Giriniz: ");
+        r = Convert.ToInt32(Console.ReadLine());
+        
+        for(i = -r; i <= r; i++){
+            for(j = -r; j <= r; j++){
+                if(i * i + j * j <= r * r){
+                    Console.Write("*");
+                }else{
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
