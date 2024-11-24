6cdd78d0d5565383f87c6ab21dafdbccbcf3c341
Ayalni2289 <alininspor@hotmail.com>
2024-11-24 15:36:34 +0300

    Ucgen Cizme Algoritması Oluşturuldu

---
+++ C# Projeleri/Ucgen Çizme/Program.cs
@@ -0,0 +1,21 @@
+﻿Ucgen ucgen = new Ucgen();
+ucgen.Yaz();
+
+class Ucgen{
+    public void Yaz(){
+        int i, j, k, n;
+        
+        Console.Write("Ucgen Yüksekliği Giriniz: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for(i = 1; i <= n; i++){
+            for(j = 1; j <= n - i; j++){
+                Console.Write(" ");
+            }
+            for(k = 1; k <= 2 * i - 1; k++){
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
